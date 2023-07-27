c46584c6ed070750e323b6a0cb1e8fe887a1198d
Pedro Nunes <pedro.nunesmoura@outlook.com>
2023-07-27 18:16:50 -0300

    Inserting plan search parameter by ID

--- routes/api.php
+++ routes/api.php
@@ -3,21 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::prefix('plans')->group(function () {
    Route::name('plans.create')->post('/', 'PlansController@create');
+   Route::name('plans.filter')->get('/', 'PlansController@filter');
 });
